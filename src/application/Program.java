c38eeae745ed85d4185713065d6f4826dad36556
Eduardo Wanderley <eduardobraz.junior@ucsal.edu.br>
2020-09-20 11:57:24 -0300

    Seller entitiy class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "bookss");
 		
-		System.out.println(department);
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, department);
+		
+		System.out.println(seller);
 
+		
 	}
 
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,13 +2,12 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable{
+public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
-	
-	
+
 	public Department() {
 	}
 
@@ -63,7 +62,5 @@ public class Department implements Serializable{
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-	
-	
-	
+
 }
